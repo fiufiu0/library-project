37bb51a5ca8700e7e6e6ea21aaa5c00d4c683eaa
lukaszgromadzki <28166634+lukaszgromadzki@users.noreply.github.com>
2023-05-03 20:04:44 +0200

    DOM selectors

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
-let books = [];
+const books = document.querySelector('#books');
+
+
+let booksArray = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+}
+
+
+let book1 = new Book("Hobbit", "Tolkien", 125, false);
+let book2 = new Book("Potter", "JK Rowling", 521, true);
+let book3 = new Book("Hobbit 2", "Tolkien", 333, true);
+
+// const addBookToLibrary = () => {
+
+// }
+
+
+const createBook = () => {
+ 
+  const booksCard = document.createElement('div');
+  const title = document.createElement('p');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const read = document.createElement('p');
+
 }
